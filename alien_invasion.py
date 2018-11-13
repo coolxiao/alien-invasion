10ee961ff69dcbbd5cef77c174dbc28021fdcf49
jiexiaoqiang <877023699@qq.com>
2018-11-13 15:03:00 +0800

    射杀敌人

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 import game_functions as gf
 
 
@@ -12,10 +13,15 @@ def rum_game():
     screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
     pygame.display.set_caption('Alien Invasion')
     ship = Ship(ai_setting, screen)
+    bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_setting, screen, ship, aliens)
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_setting, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_setting, screen, ship)
+        gf.update_bullets(bullets, aliens)
+        gf.update_aliens(ai_setting, aliens)
+        gf.update_screen(ai_setting, screen, ship, aliens, bullets)
 
 
 if __name__ == '__main__':
